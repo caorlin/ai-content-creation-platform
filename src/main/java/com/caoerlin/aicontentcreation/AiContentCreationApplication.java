18152b116c0fe8a478b713ac36cdad05c9343785
zyj <2223800485@qq.com>
2026-05-30 01:35:59 +0800

    feat：mybatis-plus配置

--- src/main/java/com/caoerlin/aicontentcreation/AiContentCreationApplication.java
+++ src/main/java/com/caoerlin/aicontentcreation/AiContentCreationApplication.java
@@ -1,11 +1,13 @@
 package com.caoerlin.aicontentcreation;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan(basePackages = "com.caoerlin.aicontentcreation.mapper")
 public class AiContentCreationApplication {
 
     public static void main(String[] args) {
