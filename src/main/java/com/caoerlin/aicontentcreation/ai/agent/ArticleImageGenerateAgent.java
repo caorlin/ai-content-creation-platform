4dc58a8c9aa8b1620354cd69e2c75759bcedcda6
caoerlin <2223800485@qq.com>
2026-06-01 06:13:53 +0800

    feat:文章配图需求agent

---
+++ src/main/java/com/caoerlin/aicontentcreation/ai/agent/ArticleImageGenerateAgent.java
@@ -0,0 +1,42 @@
+package com.caoerlin.aicontentcreation.ai.agent;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.caoerlin.aicontentcreation.ai.constant.PromptConstant;
+import com.caoerlin.aicontentcreation.ai.utils.AiModelCallingUtils;
+import com.caoerlin.aicontentcreation.ai.utils.AiResponseParseUtils;
+import com.caoerlin.aicontentcreation.model.dto.article.ArticleState;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ArticleImageGenerateAgent {
+    private final ChatModel articleContentModel;
+
+    public void analyzeArticleIllustration(ArticleState state) {
+        if (ObjectUtil.isNull(state)) {
+            log.error("生成文章内容异常,文章状态为空");
+            return;
+        }
+
+        //生成文章需要的图片关键字prompt
+        String prompt = PromptConstant.ARTICLE_IMAGE_REQUIREMENTS_AGENT_PROMPT
+                .replace("{mainTitle}", state.getTitle().getMainTitle())
+                .replace("{content}", state.getContent());
+
+        //生成图片关键字
+        String content = AiModelCallingUtils.callModel(articleContentModel, prompt);
+
+        //提取
+        List<ArticleState.ImageRequirement> imageRequirementList = AiResponseParseUtils.parseJsonListResponse(content, ArticleState.ImageRequirement.class, "文章配图需求");
+
+        state.setImageRequirements(imageRequirementList);
+
+        log.info("智能体 ArticleImageGenerateAgent 生成文章内容成功,size={}", imageRequirementList.size());
+    }
+}
--- src/main/java/com/caoerlin/aicontentcreation/ai/utils/AiResponseParseUtils.java
+++ src/main/java/com/caoerlin/aicontentcreation/ai/utils/AiResponseParseUtils.java
@@ -1,10 +1,13 @@
 package com.caoerlin.aicontentcreation.ai.utils;
 
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 import com.caoerlin.aicontentcreation.common.exception.BusinessException;
 import com.caoerlin.aicontentcreation.common.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class AiResponseParseUtils {
@@ -18,15 +21,42 @@ public class AiResponseParseUtils {
      * @return
      */
     public static <T> T parseJsonResponse(String content, Class<T> clazz, String name) {
+        hasContent(content, name);
+        try {
+            return JSONUtil.toBean(content, clazz);
+        } catch (Exception e) {
+            parseExceptionLogger(content, name, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, name + "解析失败");
+        }
+    }
+
+    /**
+     * 解析json数组
+     *
+     * @param content ai响应的内容
+     * @param clazz   要转换的类型
+     * @param name    名称
+     * @return
+     */
+    public static <T> List<T> parseJsonListResponse(String content, Class<T> clazz, String name) {
+        hasContent(content, name);
+        try {
+            JSONArray jsonArray = JSONUtil.parseArray(content);
+            return JSONUtil.toList(jsonArray, clazz);
+        } catch (Exception e) {
+            parseExceptionLogger(content, name, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, name + "解析失败");
+        }
+    }
+
+    private static void hasContent(String content, String name) {
         if (StrUtil.isBlank(content)) {
             log.error("{}解析内容为空,解析失败", name);
             throw new BusinessException(ErrorCode.PARAMS_ERROR, name + "解析内容为空,解析失败");
         }
-        try {
-            return JSONUtil.toBean(content, clazz);
-        } catch (Exception e) {
-            log.error("{}解析失败,content = {},e = {}", name, content, e.getMessage());
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR,name + "解析失败");
-        }
+    }
+
+    private static void parseExceptionLogger(String content, String name, Exception e) {
+        log.error("{}解析失败,content = {},e = {}", name, content, e.getMessage());
     }
 }
