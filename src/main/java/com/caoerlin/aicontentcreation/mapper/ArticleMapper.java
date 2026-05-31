cae0c4ee8f4eab72679bd70f282df10c5be61b53
caoerlin <2223800485@qq.com>
2026-05-31 15:45:47 +0800

    feat:修改

--- src/main/java/com/caoerlin/aicontentcreation/mapper/ArticleMapper.java
+++ src/main/java/com/caoerlin/aicontentcreation/mapper/ArticleMapper.java
@@ -4,9 +4,8 @@ import com.caoerlin.aicontentcreation.model.entity.Article;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
-* @author Administrator
+* @author zyj
 * @description 针对表【article(文章表)】的数据库操作Mapper
-* @createDate 2026-05-31 15:35:52
 * @Entity com.caoerlin.aicontentcreation.model.entity.Article
 */
 public interface ArticleMapper extends BaseMapper<Article> {
