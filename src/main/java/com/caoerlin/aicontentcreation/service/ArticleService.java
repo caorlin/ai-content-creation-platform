b5fa72810b902d7e863530e3f8f3946bf13cc546
caoerlin <2223800485@qq.com>
2026-05-31 15:43:53 +0800

    feat:修改

--- src/main/java/com/caoerlin/aicontentcreation/service/ArticleService.java
+++ src/main/java/com/caoerlin/aicontentcreation/service/ArticleService.java
@@ -4,9 +4,8 @@ import com.caoerlin.aicontentcreation.model.entity.Article;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author Administrator
+* @author zyj
 * @description 针对表【article(文章表)】的数据库操作Service
-* @createDate 2026-05-31 15:35:52
 */
 public interface ArticleService extends IService<Article> {
 
--- src/main/java/com/caoerlin/aicontentcreation/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/caoerlin/aicontentcreation/service/impl/ArticleServiceImpl.java
@@ -7,9 +7,8 @@ import com.caoerlin.aicontentcreation.mapper.ArticleMapper;
 import org.springframework.stereotype.Service;
 
 /**
-* @author Administrator
+* @author zyj
 * @description 针对表【article(文章表)】的数据库操作Service实现
-* @createDate 2026-05-31 15:35:52
 */
 @Service
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article>
