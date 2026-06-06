892210b513a3afb07cf9e906bb8013b6bb1cda08
zyj <2223800485@qq.com>
2026-06-06 23:54:15 +0800

    feat：AI 生成文章标题选择列表

---
+++ src/main/java/com/caoerlin/aicontentcreation/service/ArticleAgentService.java
@@ -0,0 +1,15 @@
+package com.caoerlin.aicontentcreation.service;
+
+import com.caoerlin.aicontentcreation.model.dto.article.ArticleState;
+
+import java.util.function.Consumer;
+
+public interface ArticleAgentService {
+    /**
+     * 阶段1：执行文章标题生成-文章标题生成阶段
+     *
+     * @param state         文章状态
+     * @param streamHandler 模型执行状态流转
+     */
+    void executeArticleTitleGeneratePhage(ArticleState state, Consumer<String> streamHandler);
+}
---
+++ src/main/java/com/caoerlin/aicontentcreation/service/impl/ArticleAgentServiceImpl.java
@@ -0,0 +1,34 @@
+package com.caoerlin.aicontentcreation.service.impl;
+
+import com.caoerlin.aicontentcreation.ai.agent.ArcticTitleAgent;
+import com.caoerlin.aicontentcreation.ai.common.enums.SseMessageTypeEnum;
+import com.caoerlin.aicontentcreation.common.exception.BusinessException;
+import com.caoerlin.aicontentcreation.common.exception.ErrorCode;
+import com.caoerlin.aicontentcreation.model.dto.article.ArticleState;
+import com.caoerlin.aicontentcreation.service.ArticleAgentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ArticleAgentServiceImpl implements ArticleAgentService {
+    private final ArcticTitleAgent arcticTitleAgent;
+
+    @Override
+    public void executeArticleTitleGeneratePhage(ArticleState state, Consumer<String> streamHandler) {
+        try {
+            log.info("文章标题方案生成阶段,开始生成标题方案,taskId={},topic={},style={}", state.getTaskId(), state.getTopic(), state.getStyle());
+            //开始生成文章标题
+            arcticTitleAgent.generateArticleTitle(state);
+            streamHandler.accept(SseMessageTypeEnum.ARTICLE_TITLE_AGENT_COMPLETE.getValue());
+            log.info("文章标题方案生成完成,taskId={},titleOptionSize={}", state.getTaskId(), state.getTitleOptions().size());
+        } catch (Exception e) {
+            log.error("文章标题方案生成失败,taskId={},e={}", state.getTaskId(), e.getMessage());
+            throw new BusinessException(ErrorCode.OPERATION_ERROR, "文章标题方案生成失败");
+        }
+    }
+}
